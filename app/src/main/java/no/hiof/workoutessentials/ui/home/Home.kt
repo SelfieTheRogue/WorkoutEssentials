d0761a74cc9ca50ccda8b066391d7e284d6890a7
elifortmort <eliot.lillevold@gmail.com>
2023-11-26 14:22:22 +0100

    Update Home.kt

--- app/src/main/java/no/hiof/workoutessentials/ui/home/Home.kt
+++ app/src/main/java/no/hiof/workoutessentials/ui/home/Home.kt
@@ -4,8 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,16 +24,58 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
+import no.hiof.workoutessentials.model.Exercise
 import no.hiof.workoutessentials.service.StorageService
 import no.hiof.workoutessentials.service.api.ApiViewModel
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
+@Composable
+fun AddDetailDialog(
+    exercise: Exercise,
+    onDismiss: () -> Unit,
+) {
+    Dialog(onDismissRequest = onDismiss)
+    {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(600.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+
+
+            Column() {
+                Text(text = "Name: " + exercise.name)
+                Text(text = "Muscle Group: " + exercise.muscle)
+                Text(text = "Type: " + exercise.type)
+                Text(text = "Difficulty: " + exercise.difficulty)
+                Text(text = "Equipment: " + exercise.equipment)
+                Text(text = "Instructions")
+            }
+            Divider(thickness = 2.dp, color = Color.Black)
+            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = exercise.instructions, modifier = Modifier
+                    .verticalScroll(
+                        rememberScrollState()
+                    )
+                    .weight(5F, false))
+
+                androidx.compose.material3.Button(onClick = onDismiss) {
+                    Text(text = "Exit")
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun Home(storageService: StorageService) {
@@ -35,6 +83,10 @@ fun Home(storageService: StorageService) {
     val viewModel: ApiViewModel = viewModel()
 
     var buttonClicked by remember { mutableStateOf(false) }
+    var showDetail by remember { mutableStateOf(false) }
+    var selectedExercise by remember { mutableStateOf("")}
+
+    var exerciseList by remember { mutableStateOf<List<Exercise>>(emptyList()) }
 
     val date = LocalDate.now()
     val formatter = DateTimeFormatter.ofPattern("EEEE")
@@ -47,6 +99,7 @@ fun Home(storageService: StorageService) {
         date.plusDays(5),
         date.plusDays(6)
     )
+    var selectedDay by remember { mutableStateOf(date.format(formatter)) }
 
     fun urlGen (exercises: List<String>): String {
         var url = ""
@@ -63,7 +116,28 @@ fun Home(storageService: StorageService) {
 
     val exercisesState = remember { mutableStateOf<List<String>>(emptyList()) }
 
-    var selectedDay by remember { mutableStateOf(date.format(formatter)) }
+    LaunchedEffect(Unit) {
+        try {
+            val exerciseListFromFire = storageService.getExercises(selectedDay.replaceFirstChar {
+                it.lowercase(
+                    Locale.ROOT
+                )
+            })
+            exercisesState.value = exerciseListFromFire
+        } catch (e: Exception) {
+
+        }
+    }
+
+    LaunchedEffect(showDetail, selectedExercise) {
+        if (showDetail) {
+            try {
+                viewModel.fetchData("exercises?name=$selectedExercise")
+            } catch (e: Exception) {
+
+            }
+        }
+    }
 
     LaunchedEffect(buttonClicked, selectedDay) {
         if (buttonClicked) {
@@ -94,7 +168,22 @@ fun Home(storageService: StorageService) {
         onDispose { }
     }
 
+    data?.let {
+        exerciseList = it
+    }
 
+    if(showDetail) {
+        if(selectedExercise != null) {
+            for (item in exerciseList) {
+                if(item.name == selectedExercise)    {
+                    AddDetailDialog(
+                        exercise = item,
+                        onDismiss = { showDetail = false }
+                    )
+                }
+            }
+        }
+    }
 
     Row (modifier = Modifier.fillMaxSize()){
         Column (horizontalAlignment = Alignment.CenterHorizontally,
@@ -122,18 +211,18 @@ fun Home(storageService: StorageService) {
             modifier = Modifier.weight(1F)) {
             Text(selectedDay,
                 style = MaterialTheme.typography.headlineLarge)
-            Column (horizontalAlignment = Alignment.CenterHorizontally){
+            Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                .verticalScroll(
+                    rememberScrollState()
+                )
+                .weight(1F, false)){
                 for (exercise in exercisesState.value) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(text = exercise.replace("_", " "))
-                    }
-                }
-                data?.let {
-                    for (exercise in it) {
-                        if(exercise.name == "Landmine twist"){
-                            Text(text = exercise.name)
+                        Button(onClick = { showDetail = true
+                                         selectedExercise = exercise}, modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)) {
+                            Text(text = exercise.replace("_", " "))
                         }
-                    }
                 }
             }
         }
